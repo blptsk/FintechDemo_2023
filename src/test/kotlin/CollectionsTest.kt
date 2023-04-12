6a60acacf1ba52fd686560b9527b0f8224977719
a.belolipetskiy <a.belolipetskiy@tinkoff.ru>
2023-04-12 12:35:43 +0300

    add junit test example

---
+++ src/test/kotlin/CollectionsTest.kt
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+class CollectionsTest {
+
+    @Test
+    fun testFunc(){
+        assertEquals(message = "Error multiply 4 * 4", expected = 16, actual = 4 * 3)
+    }
+}
